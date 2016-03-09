551f6288756f0b39bcc975dd4b89364685ae0236
Matt Hays <mdhays1995@gmail.com>
2016-03-09 17:33:22 -0600

    Querying from database on route load.

--- server.js
+++ server.js
@@ -3,7 +3,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const pg = require('pg').native
+const server = require('http').createServer(app)
 const port = process.env.PORT || 3000
+const POSTGRES_URL = process.env.POSTGRES_URL
+  || 'postgres://localhost:5432/wyvern'
+
+let db
+
 
 app.set('view engine', 'jade')
 
@@ -27,6 +34,23 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, () => {
-  console.log(`wyvern running on ${port}`)
+app.get('/songs', (req, res) => {
+  db.query('SELECT * FROM songs', (err, result) => {
+    if(err) throw err
+
+    res.send(result.rows)
+  })
 })
+
+
+
+pg.connect(POSTGRES_URL, (err, client) => {
+
+  db = client
+
+  server.listen(port, () => {
+    console.log(`wyvern running on ${port}`)
+  })
+})
+
+
