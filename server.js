c071178fc5a5c2e31731d1f59dfc9b860f287367
Matt Hays <mdhays1995@gmail.com>
2016-03-08 21:06:28 -0600

    Set up basic dependencies, and got sass compiling.

--- server.js
+++ server.js
@@ -2,9 +2,25 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'jade')
+
+
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}))
+
+
+app.use(express.static('public'))
+
+
+
+
 
 app.get('/', (req, res) => {
   res.render('index')
