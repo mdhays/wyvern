a76b51118534fc237d2b9b5c8f2449b927af9493
Matt Hays <mdhays1995@gmail.com>
2016-03-14 12:09:26 -0500

    Form now writes to database. Body-parser is now being used.

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 'use strict';
 
-const express = require('express')
-const app = express()
-const path = require('path')
-const pg = require('pg').native
-const server = require('http').createServer(app)
-const port = process.env.PORT || 3000
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const path = require('path');
+const pg = require('pg').native;
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
 const POSTGRES_URL = process.env.POSTGRES_URL
-  || 'postgres://localhost:5432/wyvern'
+  || 'postgres://localhost:5432/wyvern';
 
-let db
+let db;
 
 
-app.set('view engine', 'jade')
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(require('node-sass-middleware')({
@@ -20,10 +23,10 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true,
   sourceMap: true
-}))
+}));
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 
 
@@ -31,26 +34,28 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
-})
+});
 
-
-app.get('/songs', (req, res) => {
-  db.query('SELECT * FROM songs', (err, result) => {
+app.post('/', (req, res) => {
+  db.query(`INSERT INTO chatlog (message) VALUES ('${req.body.msg}')`, (err, result) => {
     if(err) throw err
+    console.log(req.body.msg);
+    res.send(req.body.msg);
+  });
+});
 
-    res.send(result.rows)
-  })
-})
+
+
 
 
 
 pg.connect(POSTGRES_URL, (err, client) => {
 
-  db = client
+  db = client;
 
   server.listen(port, () => {
-    console.log(`wyvern running on ${port}`)
-  })
-})
+    console.log(`wyvern running on ${port}`);
+  });
+});
 
 
