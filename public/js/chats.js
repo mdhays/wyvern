d2b2d86c61026a01f861933e2c5767113d105932
Matt Hays <mdhays1995@gmail.com>
2016-03-17 10:45:42 -0500

    Websockets are now working, past messages are now showing.

--- public/js/chats.js
+++ public/js/chats.js
@@ -1,57 +1,83 @@
+'use strict';
+
 $(document).ready(function() {
 
-  'use strict';
+
+      const ul = document.querySelector('ul');
+    const username = $('#name');
+    const text = $('#msg');
+
+
+    const ws = io.connect();
+
+
+  ws.on('connect', (socket) => {
+    console.log('socket connected')
+  });
+
+   const chat = {
+      username: username.val(),
+      text: text.val()
+    };
+
+
+  ws.on('receiveChat', (msgs) => {
+    msgs.forEach(displayChat)
+  });
+
+      function displayChat (chat) {
+      
+        const li = generateLI(chat)
+
+        ul.appendChild(li)
+    }
+
+
+        function generateLI (chat) {
+      const li = document.createElement('li')
+      const textNode = document.createTextNode(`${chat.username}: ${chat.message}`)
+      console.log(chat);
+      const dataId = document.createAttribute('data-id')
+
+      dataId.value = chat._id
+
+      li.setAttributeNode(dataId)
+      li.appendChild(textNode)
+
+      return li
+    }
+
 
   // jquery click function to be run on each.
   $('#send').click(function() {
 
         
-        const ul = document.querySelector('ul');
-        const username = $('#name');
-        const text = $('#msg');
-        
-        
 
-        const chat = {
-          username: username.val(),
-          text: text.val()
-        };
-        
+    
+    
 
-        function displayChat (chat) {
-          
-            const li = generateLI(chat)
-
-            ul.appendChild(li)
-        }
-
-        displayChat(chat)
-
-        function generateLI (chat) {
-          const li = document.createElement('li')
-          const textNode = document.createTextNode(`${chat.username}: ${chat.text}`)
-          console.log(chat.username);
-          const dataId = document.createAttribute('data-id')
-
-          dataId.value = chat._id
-
-          li.setAttributeNode(dataId)
-          li.appendChild(textNode)
-
-          return li
-        }
+   
 
 
-        
-        $.ajax({
-          type: 'POST',
-          url: '/',
-          data: chat,
-          success: 'success',
-          dataType: 'json'
-        });
+    ws.emit('sendChat', chat => {
+      displayChat(chat)
+      text.value = ''
+    });
+    
+    
 
+
+
+
+
+
+    
+    $.ajax({
+      type: 'POST',
+      url: '/',
+      data: chat,
+      success: 'success',
+      dataType: 'json'
+    });
   });
-
-
 });
--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 const path = require('path');
 const pg = require('pg').native;
 const server = require('http').createServer(app);
+const ws = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 const POSTGRES_URL = process.env.POSTGRES_URL
   || 'postgres://localhost:5432/wyvern';
 
-let db;
+let db = new pg.Client(POSTGRES_URL);
 
 
 app.set('view engine', 'jade');
@@ -39,23 +40,47 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log(req)
-  db.query(`INSERT INTO chatlog (message) VALUES ('${req.body.text}')`, (err, result) => {
-    if(err) throw err
-    console.log(req.body.msg);
-    res.send(req.body.msg);
-  });
+  // db.query(`INSERT INTO chatlog (message) VALUES ('${req.body.text}')`, (err, result) => {
+  //   if(err) throw err
+  //   console.log(req.body.msg);
+  //   res.send(req.body.msg);
+  // });
 });
 
 
 
 
+db.connect((err) => {
+  if (err) throw err
 
+  pg.connect(POSTGRES_URL, (err, client) => {
 
-pg.connect(POSTGRES_URL, (err, client) => {
+    db = client;
 
-  db = client;
+    server.listen(port, () => {
+      console.log(`wyvern running on ${port}`);
+    });
+  });
 
-  server.listen(port, () => {
-    console.log(`wyvern running on ${port}`);
+  ws.on('connection', socket => {
+  console.log('socket connected', socket.id)
+
+  db.query('SELECT * FROM chatlog', (err, result) => {
+    if (err) throw err;
+
+    socket.emit('receiveChat', result.rows);
+  });
+
+  socket.on('sendChat', msg => {
+    db.query(`INSERT INTO chatlog (message) VALUES ('${req.body.text}')`,
+      [msg.name, msg.text], (err) => {
+        if (err) throw err;
+        // Broadcast emits to all but this socket.
+        socket.broadcast.emit('receiveChat', [msg]);
+      });
+    });
   });
 });
+
+
+
