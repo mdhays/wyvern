ea650b4a7a46d6d38e92dcbcfc3436fef8ff5bb3
Matt Hays <mdhays1995@gmail.com>
2016-03-21 21:30:58 -0500

    Song names from search are now displaying.

--- public/js/music.js
+++ public/js/music.js
@@ -1,21 +1,42 @@
 'use strict';
 $(document).ready(function() {
 
+const client_id = '';
+const base = '//api.soundcloud.com/tracks?linked_partitioning=1&client_id=';
+const options = '&limit=20&offset=0&q=';
 
-  SC.initialize({
-    client_id: '',
-    redirect_uri: 'http://localhost:3000/callback'
+const scUrl = `${base}${client_id}${options}`;
+
+
+
+  $('#find').click(function() {
+
+    const song = $('#song').val()
+    console.log('running', song);
+    $.get(`${scUrl}${song}`, function(data, status) {
+      const results = data.collection;
+      
+      
+      for(let i = 0; i < results.length; i++) {
+        const output = document.getElementById('output');
+        console.log(results[i]);
+        const li = generateLI(results[i]);
+        
+        output.appendChild(li);
+
+      }
+
+    function generateLI (result) {
+      const li = document.createElement('li');
+      const textNode = document.createTextNode(`${result.title}`);
+
+      li.appendChild(textNode);
+      console.log(li);
+      return li;
+    };
+
+    });
   });
-    
-   // initiate auth popup
-  SC.connect().then(function() {
-    return SC.get('/me');
-  }).then(function(me) {
-    alert('Hello, ' + me.username);
-    console.log(me);
-  });
-
-
 });
 
 
