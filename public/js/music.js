71e69ad9ecd5c2dff95fd0c67f80e64722559fed
Matt Hays <mdhays1995@gmail.com>
2016-03-22 22:08:00 -0500

    Searching for a song/artist and clicking on the title plays the track.

--- public/js/music.js
+++ public/js/music.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   const client_id = 'e5b95dafbd80c22b802bfa85f59e5073';
   const base = '//api.soundcloud.com/tracks?linked_partitioning=1&client_id=';
   const options = '&limit=20&offset=0&q=';
+  let songURI;
+
 
   // Concatenates the url for the search.
   const scUrl = `${base}${client_id}${options}`;
@@ -13,18 +15,18 @@ $(document).ready(function() {
 
   // Loads up the soundcloud widget and listens for events.
   //////////////////////////////////////////////////
-  const widget = SC.Widget(document.getElementById('soundcloud_widget'));
+  // const widget = SC.Widget(document.getElementById('soundcloud_widget'));
   
 
 
-  widget.bind(SC.Widget.Events.READY, function() {
-    console.log('Ready...');
-  });
+  // widget.bind(SC.Widget.Events.READY, function() {
+  //   console.log('Ready...');
+  // });
   
 
-  widget.bind(SC.Widget.Events.FINISH, function() {
-    changeIframeSrc();
-  });
+  // widget.bind(SC.Widget.Events.FINISH, function() {
+  //   changeIframeSrc();
+  // });
    
   //////////////////////////////////////////////////
 
@@ -33,21 +35,22 @@ $(document).ready(function() {
   function changeIframeSrc() {
 
     const prependStreamURL = 'http://w.soundcloud.com/player/?url=';
-    const test = 'https://api.soundcloud.com/tracks/31204641';
+    // const test = 'https://api.soundcloud.com/tracks/31204641';
     const displayAs = '&show_artwork=true&liking=true&sharing=true&auto_play=true';
 
-    document.getElementById('soundcloud_widget').src = `${prependStreamURL}${test}${displayAs}`;
+    document.getElementById('soundcloud_widget').src = `${prependStreamURL}${songURI}${displayAs}`;
 
   }
 
 
 
- 
-
-
-
-
-
+  $('#output').click(function() {
+    $('.song').click(function() {
+      songURI = this.id;
+      console.log(songURI);
+      changeIframeSrc(songURI);
+    });
+  });
 
 
 
@@ -57,46 +60,55 @@ $(document).ready(function() {
 
     const song = $('#song').val()
     
-    $.get(`${scUrl}${song}`, function(data, status) {
+      $.get(`${scUrl}${song}`, function(data, status) {
       
-      const results = data.collection;
-      
-      let counter = 0;
-
-      for(let i = 0; i < results.length; i++) {
-        const output = document.getElementById('output');
-        console.log(results[i]);
-        const li = generateLI(results[i]);
+        const results = data.collection;
         
-        output.appendChild(li);
+        let counter = 0;
 
+        for(let i = 0; i < results.length; i++) {
+          
+          const output = document.getElementById('output');
+          console.log(results[i], "looping through data array");
+          
+          let uri = getURI(results[i]);
+          console.log(uri);
+          const button = generateButton(results[i]);
+          
+
+          output.appendChild(button);
+
+        }
+
+
+      function getURI(song) {
+        let uri = song.uri;
+        return uri;
       }
 
-    function generateLI (result) {
 
-      const li = document.createElement('li');
-      const textNode = document.createTextNode(`${result.title}`);
+      function generateButton (result) {
 
-      const dataId = document.createAttribute('data-id');
+        const button = document.createElement('button');
+        const textNode = document.createTextNode(`${result.title}`);
+        const Class = document.createAttribute('class');
+        const id = document.createAttribute('id');
 
-      let i = 0;
-      dataId.value = counter++;
 
-      li.setAttributeNode(dataId);
-      li.appendChild(textNode);
-      return li;
-    };
+        
+        id.value = result.uri;
+        Class.value = 'song';
+        button.setAttributeNode(id);
+        button.setAttributeNode(Class);
+        button.appendChild(textNode);
+        return button;
+      };
 
     });
+
   });
+
+
 });
-
-
-
-
-
-
-// 1. Get the uri of a sound by clicking on the songname.
-// 2. Load the new sound into the widget.
-// 3. If user wants to add more, push uri's into an array.
-// 4. On SC.Widget.Events.FINISH, load the next uri from the array into the widget.
+  
+// 1. Get the uris and push them into a table.
